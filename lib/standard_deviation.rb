9dd0cc88fe304b843df090f48c61423c94c7461e
Colin Alexander <colintalex@gmail.com>
2020-01-30 16:06:54 -0700

    Adds complete, functional standard_deviation function

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -3,13 +3,12 @@ ages = [24, 30, 18, 20, 41]
 total_age = 0.00
 mean_age = 0.00
 
-answer = 0.00
-
 to_be_squared = 0.00
 squared_result = 0.00
 squared_array = []
 total_of_squared_array = 0.00
 mean_of_squared_array = 0.00
+answer = 0.00
 
 # FIND THE AVERAGE OF AGES
   ages.each do |number|
@@ -22,6 +21,7 @@ mean_of_squared_array = 0.00
     to_be_squared = number - mean_age
     squared_result = to_be_squared * to_be_squared
     squared_array << squared_result
+
 #FIND MEAN OF NEW ARRAY
   end
   squared_array.each do |number|
@@ -31,8 +31,3 @@ mean_of_squared_array = 0.00
 
 answer = Math.sqrt(mean_of_squared_array)
 puts answer.round(2)
-#1 AVerage of numbers
-#2 For each number, subtract mean and square result
-#3 Find the mean of the squared results
-# Take square root of results. Done!
-# When you find the standard deviation, print it out
